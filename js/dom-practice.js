fdefb680e4f379b8af091dddaa3bbeb3673f1a2e
annielly <46509197+annielly@users.noreply.github.com>
2019-04-04 14:51:04 -0700

    Add files via upload
    Part 1 & 2

---
+++ js/dom-practice.js
@@ -0,0 +1,55 @@
+/*eslint-env browser*/
+
+//STEP 1
+function callMessage() {
+    "use strict";
+    window.alert("I have been clicked.")
+}
+
+//STEP 2
+window.document.getElementById("step2").onclick = callMessage;
+
+//STEP 3
+window.document.getElementById("step3").addEventListener("click", callMessage);
+
+//STEP 4
+step4.addEventListener("click", function () {
+    window.alert("I have been clicked.");
+});
+
+//STEP 5
+
+//STEP 6
+document.getElementById("redirect").addEventListener("click", function(e) {
+    e.preventDefault();
+    window.alert("I have clicked on the Google link, but all I get is this pop-up!");
+});
+
+//STEP 7
+window.document.getElementById("step7").addEventListener("click", function(e) {
+    window.document.getElementById("step7").disabled = true;
+    var popup = window.document.getElementById("text").value;
+    window.alert(popup);
+})
+
+//STEP 8
+window.document.getElementById("step8").addEventListener("click", function () {
+    newPage();
+});
+function newPage() {
+  newWindow = window.open("newpage.html", "", "width=300, height=300");
+    newWindow.addEventListener("load", function () {
+        resizeWindow();
+    })
+}
+function resizeWindow() {
+    newWindow.resizeTo(500, 300);
+}
+
+//STEP 9
+
+//STEP 10
+window.document.getElementById("step10").addEventListener("click", function () {
+    "use strict";
+    window.alert(document.getElementById("menu").value);
+});
